491af9a0dc57ef3ae9f49811646110cdeedd7a32
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-08-02 21:13:53 +0530

    Finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const letters = word.split("");
+    return letters.reverse().join("");
 };
 
 // Do not edit below this line

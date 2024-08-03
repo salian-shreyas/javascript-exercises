2b95184867431806dea5cb5bebb9618553bebd05
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-08-03 19:03:52 +0530

    Finish exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    let valInCelsius =  5/9 * (fahrenheit - 32);
+    return Math.round( valInCelsius * 10 ) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    let valInFahrenheit = 9 / 5 * celsius + 32;
+    return Math.round( valInFahrenheit * 10 ) / 10;
 };
 
 // Do not edit below this line

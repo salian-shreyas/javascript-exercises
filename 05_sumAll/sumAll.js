e11358860daec5e3d755fbd8c32581b4f0df5200
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-08-03 17:49:51 +0530

    Finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
-};
+    // Sum only positive integers
+    if ( Number.isInteger(start) 
+        && Number.isInteger(end) 
+        && start >= 0 
+        && end >= 0
+    ) {
+        if ( end < start ) {
+            // When the end is lesser than start, swap
+            let temp = start;
+            start = end;
+            end = temp;
+        }
+
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    return "ERROR";
+  };
 
 // Do not edit below this line
 module.exports = sumAll;

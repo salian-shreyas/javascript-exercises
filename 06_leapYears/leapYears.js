abcc3e86f11899a66d001c6e8ac47130a90a4d48
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-08-03 18:36:38 +0530

    Finish exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,7 @@
 const leapYears = function(year) {
-    if ( year % 4 === 0) {
-        if ( year % 100 === 0 && year % 400 !== 0) return false;
-
-        return true;
-    }
-
-    return false;
+   if ((year % 4 !== 0) || (year % 100 === 0 && year % 400 !== 0)) return false;
+   
+   return true;
 };
 
 // Do not edit below this line
